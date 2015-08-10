2d51c50d6fb6d9437937226deb87afd0b547ff7d
Guilherme Cavichioli <guilhermecavichioli@hotmail.com>
2015-08-10 08:18:45 -0300

    Separating sidebar to his file
    Sidebar css into his sass file
    
    Fix:
    - When sidebar open, dark (and clickable) background added
    - Small logo on sidebar now is a link to the home screen

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,3 +4,7 @@ var $html = document.querySelector('html');
 $toggle.onclick = function(e) {
   $html.classList.toggle('open');
 };
+
+document.onclick = function(e) {
+  $html.classList.contains('open') && e.target.tagName == 'BODY' && $html.classList.remove('open');
+};
